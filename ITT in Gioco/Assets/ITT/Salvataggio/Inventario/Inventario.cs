184bcff709eb1d90b4b45575c3a51ac6c89e6b8a
ProgettoScuola <ittingioco@gmail.com>
2018-05-21 15:58:53 +0200

    aggiunta parte salvataggio info di base quali: posizione rotazione e variabili di prova

---
+++ ITT in Gioco/Assets/ITT/Salvataggio/Inventario/Inventario.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace ITT.Salvataggio.Inventario
+{
+    public class Inventario
+    {
+        public Inventario()
+        {
+
+        }
+    }
+}
---
+++ ITT in Gioco/Assets/ITT/Salvataggio/Inventario/Zaino.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace ITT.Salvataggio.Inventario
+{
+    public class Zaino
+    {
+
+        public Zaino()
+        {
+
+        }
+
+        private class Items
+        {
+
+            private Items()
+            {
+
+            }
+        }
+    }
+}
