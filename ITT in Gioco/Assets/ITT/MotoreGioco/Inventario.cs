1e823f55d6d8a57805162481971edab9d6c7a6f0
PartyPancakes16 <36924608+PartyPancakes16@users.noreply.github.com>
2018-05-30 12:50:18 +0200

    Riaggiunti Script

---
+++ ITT in Gioco/Assets/ITT/MotoreGioco/Inventario.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+namespace ITT.MotoreGioco
+{
+
+    public class Inventario
+    {
+
+
+        public float maxPeso = 50f;
+        public List<OggettoInventario> listaOggettiInventario;
+
+
+        public Inventario()
+        {
+            listaOggettiInventario = new List<OggettoInventario>();
+        }
+
+        public bool rimuoviOggettoInventario(OggettoInventario o)
+        {
+           return listaOggettiInventario.Remove(o);
+        }
+
+        public OggettoInventario cercaIdOggetto (int id)
+        {
+            foreach (OggettoInventario o in listaOggettiInventario)
+            {
+                if(id == o.id)
+                {
+                    return o;
+                }
+            }
+            return null;//ritorna valore null se non trova l'id ricercato.
+        }
+
+
+
+
+        public OggettoInventario aggiungiOggetto(OggettoInventario o)
+        {
+            if(getSommmaPesiOggettiInventario() + o.peso  < maxPeso) {
+                listaOggettiInventario.Add(o);
+                return o;
+            }
+            else
+            {
+                return null;//torna valore null perche ha raggiunto il peso massimo.
+            }
+            
+        }
+
+
+        public float getSommmaPesiOggettiInventario()
+        {
+            float somma = 0f;
+            foreach(OggettoInventario o in listaOggettiInventario)
+            {
+                somma = somma + o.peso;
+            }
+            return somma;
+        }
+    }
+}
