f9f29096246c9e8aeb9eb989e9de5da7b67354a9
PartyPancakes16 <aniellocarotenuto12@gmail.com>
2018-11-11 21:59:27 +0100

    Modifiche script

--- ITT in Gioco/Assets/movimento camera automatica/PathFollower.cs
+++ ITT in Gioco/Assets/movimento camera automatica/PathFollower.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class PathFollower : MonoBehaviour {
 
 	public float speed = 3f;
 	public Transform pathParent;
 	Transform targetPoint;
-	int index;
+	int index,i=0;
+    //public GameObject cam1, cam2;
 
 	void OnDrawGizmos()
 	{
@@ -24,17 +26,30 @@ public class PathFollower : MonoBehaviour {
 		index = 0;
 		targetPoint = pathParent.GetChild (index);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		transform.position = Vector3.MoveTowards (transform.position, targetPoint.position, speed * Time.deltaTime);
-		//while(index<10){	
-			if (Vector3.Distance (transform.position, targetPoint.position) < 0.1f) 
-				{
-					index++;
-					index %= pathParent.childCount;
-					targetPoint = pathParent.GetChild (index);
-				}
-	//}
-}
+
+    void Update () {
+        //StartCoroutine(Posizione());
+        transform.position = Vector3.MoveTowards(transform.position, targetPoint.position, speed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, targetPoint.position) < 0.1f && index < 1)
+        {
+            index++;
+            index %= pathParent.childCount;
+            targetPoint = pathParent.GetChild(index);
+            Debug.Log(index);
+            /*for (int a=0;a < 1 ;a++ )
+            {
+                i++;
+                if (i == 2)
+                {
+                    yield return new WaitForSeconds(2);
+                    break;
+                }
+            }*/
+            /*if (index == 10)
+            {
+                cam1.SetActive(false);
+                cam2.SetActive(true);
+            }*/
+        }
+    }
 }
