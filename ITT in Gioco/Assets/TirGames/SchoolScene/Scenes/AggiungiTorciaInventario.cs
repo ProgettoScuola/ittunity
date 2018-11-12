88be8b35fdbd2661e93c55ee9d9986e8ba6f530e
PartyPancakes16 <aniellocarotenuto12@gmail.com>
2018-11-12 11:03:33 +0100

    Modificati script batterie

--- ITT in Gioco/Assets/TirGames/SchoolScene/Scenes/AggiungiTorciaInventario.cs
+++ ITT in Gioco/Assets/TirGames/SchoolScene/Scenes/AggiungiTorciaInventario.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AggiungiTorciaInventario : MonoBehaviour {
     public GameObject TestoTorcia, Torcia, TorciaPlayer, Bottone, Luce, Luce2;
+    public GameObject findtorcia;
     public void AggiungiInventario()
     {
         TestoTorcia.SetActive(true);
+        findtorcia.SetActive(true);
+        StartCoroutine(Disattiva());
     }
     public void NascondiTorcia()
     {
@@ -15,5 +19,10 @@ public class AggiungiTorciaInventario : MonoBehaviour {
             Bottone.SetActive(false);
             Luce.SetActive(false);
             Luce2.SetActive(false);
+    }
+    IEnumerator Disattiva()
+    {
+        yield return new WaitForSeconds(5);
+        findtorcia.SetActive(false);
     }
 }
